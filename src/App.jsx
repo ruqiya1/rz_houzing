9de9df8dc0730977d281809c5e94b0f45c751230
ruqiya1 <kamolazohidova04@gmail.com>
2022-10-01 04:12:29 +0500

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,84 @@
+import React from "react";
+import { data } from './mock.jsx'
+import { Tarzan, Container, Promo, PromoHead } from './styled.js'
+
+class App extends React.Component {
+  state = {
+    list: data,
+    selected: {}
+  }
+  render() {
+    const Delete = (id) => {
+      let res = this.state.list.filter(v => v.id != id)
+      this.setState({ list: res })
+    }
+
+    const Edit = (selected) => {
+      this.setState({ selected })
+    }
+
+    const Save = () => {
+      let res = this.state.list.map(vl => vl.id == this.state.selected.id ? this.state.selected : vl)
+      this.setState({ list: res, selected: null })
+    }
+
+    console.log(this.state.selected);
+    return (
+      <Tarzan>
+        <div>
+          {
+            this.state.list.map((v) => {
+              let slc = this.state.selected?.id == v.id
+              return <Container>
+                <img src={v.img} />
+                <Container.heroWrap>
+                 <Container.TitleWrapFlex>
+                    <Container.TitleWrap>
+                      <Container.Title>{slc ? <input defaultValue={this.state.selected.title} onChange={(e) => { this.setState({ selected: { ...this.state.selected, title: e.target.value } }) }} /> : v.title}</Container.Title>
+                      <Container.Text>{slc ? <input defaultValue={this.state.selected.text} onChange={(e) => { this.setState({ selected: { ...this.state.selected, text: e.target.value } }) }} /> : v.text}</Container.Text>
+                    </Container.TitleWrap>
+                    <Container.Btn>{v.btn}</Container.Btn>
+                  </Container.TitleWrapFlex>
+                  <Container.Price>{slc ? <input defaultValue={this.state.selected.price} onChange={(e) => { this.setState({ selected: { ...this.state.selected, price: e.target.value } }) }} /> : v.price}</Container.Price>
+                  <Container.Sum>{slc ? <input defaultValue={this.state.selected.sum} onChange={(e) => { this.setState({ selected: { ...this.state.selected, sum: e.target.value } }) }} /> : v.sum}</Container.Sum>
+                </Container.heroWrap>
+              </Container>
+            })
+          }
+        </div>
+        <table>
+          <PromoHead>
+            <PromoHead.Info>Date Published</PromoHead.Info>
+            <PromoHead.Info>Status</PromoHead.Info>
+            <PromoHead.Info>View</PromoHead.Info>
+            <PromoHead.Info>Action</PromoHead.Info>
+          </PromoHead>
+          {
+            this.state.list.map(v => {
+              let slc = this.state.selected?.id == v.id
+              return <Promo>
+                <Promo.Info>{v.data}</Promo.Info>
+                <Promo.Info>{v.status}</Promo.Info>
+                <Promo.Info>{v.view}</Promo.Info>
+                <Promo.btnWrap>
+                  {slc ? (
+                    <>
+                      <Promo.btn onClick={Save} >Save</Promo.btn>
+                      <Promo.btn onClick={() => this.setState({ selected: null })} >Cansel</Promo.btn>
+                    </>
+                  ) : <>
+                    <Promo.btn onClick={() => Edit(v)} >Edit</Promo.btn>
+                    <Promo.btn onClick={() => Delete(v.id)}>Delete</Promo.btn>
+                  </>
+                  }
+                </Promo.btnWrap>
+              </Promo>
+            })
+          }
+        </table>
+      </Tarzan>
+    );
+  };
+}
+
+export default App;
---
+++ src/styled.js
@@ -0,0 +1,117 @@
+import styled from 'styled-components'
+
+export const Container = styled.div`
+display: flex;
+align-items:center;
+height: 100px;
+margin: 20px 90px 60px 0;
+`;
+
+export const Tarzan = styled.div`
+display: flex;
+margin : 50px;
+`;
+
+Container.heroWrap = styled.h2`
+margin-top: -20px;
+margin-left: 20px;
+`;
+
+Container.Title = styled.h2`
+margin-top: 0;
+margin-bottom: 4px;
+font-weight: 600;
+font-size: 16px;
+line-height: 24px;
+color: #0D263B;
+`;
+
+Container.Text = styled.h2`
+margin: 0;
+font-weight: 400;
+font-size: 14px;
+line-height: 20px;
+color: #696969;
+`;
+
+Container.Price = styled.h2`
+margin:0;
+margin-top: -20px;
+font-weight: 400;
+font-size: 12px;
+line-height: 20px;
+text-decoration-line: line-through;
+color: #696969;
+`;
+
+Container.Sum = styled.h2`
+margin:0;
+font-weight: 600;
+font-size: 16px;
+line-height: 24px;
+color: #696969;
+`;
+
+Container.TitleWrapFlex = styled.h2`
+display:flex;
+align-items:center;
+margin:0 ;
+`;
+
+Container.TitleWrap = styled.h2`
+margin-right: 24px;
+`;
+
+Container.Btn = styled.h2`
+margin-top: -10px;
+font-weight: 400;
+font-size: 10px;
+padding: 5px 12px;
+background-color: #0D263B;
+color: #fff;
+text-transform:uppercase;
+border-radius: 3px;
+`;
+
+
+export const Promo = styled.tbody`
+width: 530px;
+height: 160px;
+display: flex;
+align-items: center;
+justify-content: space-between;
+`;
+
+export const PromoHead = styled.tbody`
+width: 500px;
+display: flex;
+align-items: center;
+justify-content: space-between;
+`;
+
+PromoHead.Info = styled.th`
+font-weight: 600;
+font-size: 18px;
+line-height: 28px;
+color: #0D263B;
+`;
+
+Promo.Info = styled.td`
+font-weight: 400;
+font-size: 14px;
+line-height: 20px;
+color: #696969;
+`;
+
+Promo.btnWrap = styled.td`
+display: flex;
+`;
+Promo.btn = styled.button`
+font-weight: 400;
+font-size: 14px;
+line-height: 20px;
+color: blue;
+margin-right: 10px;
+background-color: transparent;
+border: none;
+`;

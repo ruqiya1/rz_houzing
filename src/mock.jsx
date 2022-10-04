082cc5f7fa177c7416b1f95d5e3ae0a34edb14b4
ruqiya1 <kamolazohidova04@gmail.com>
2022-10-04 11:40:41 +0500

    Add Edit function

--- src/mock.jsx
+++ src/mock.jsx
@@ -1,5 +1,7 @@
 import img from './assets/img/img.svg'
+
 export const data = [
     {id: 1, img: img, title: 'New Apartment Nice Wiew', text: 'Quincy St, Brooklyn, NY, USA', price: '$2,800/mo', sum: '$7,500/mo', btn: 'For Sale', data: '30 December 2022', status: 'Pending', view: 5933 },
     {id: 2, img: img, title: 'New Apartment Nice Wiew', text: 'Quincy St, Brooklyn, NY, USA', price: '$2,800/mo', sum: '$7,500/mo', btn: 'For Sale', data: '30 December 2022', status: 'Pending', view: 5933 },
+    {id: 3, img: img, title: 'New Apartment Nice Wiew', text: 'Quincy St, Brooklyn, NY, USA', price: '$2,800/mo', sum: '$7,500/mo', btn: 'For Sale', data: '30 December 2022', status: 'Pending', view: 5933 }
 ]
